0c44cc232accac961f398dfbf880b1dc87d1704c
patelayush9876 <patelayush9876@gmail.com>
2025-07-20 11:47:09 +0530

    Initial Commit

---
+++ BlogNest/BlogNestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlogNestApp.swift
+//  BlogNest
+//
+//  Created by Ayush Patel on 20/07/25.
+//
+
+import SwiftUI
+
+@main
+struct BlogNestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlogNestTests/BlogNestTests.swift
@@ -0,0 +1,17 @@
+//
+//  BlogNestTests.swift
+//  BlogNestTests
+//
+//  Created by Ayush Patel on 20/07/25.
+//
+
+import Testing
+@testable import BlogNest
+
+struct BlogNestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
